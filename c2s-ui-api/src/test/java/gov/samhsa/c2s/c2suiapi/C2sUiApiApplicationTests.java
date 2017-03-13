784278503f0aaa0dadc00b2bdcf090e44229d6da
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-13 15:28:05 -0400

    Create initial project for c2s-ui-api
    #12908: As a developer, I will have integrated the componentized UI with the Backend [3.0.0]

---
+++ c2s-ui-api/src/test/java/gov/samhsa/c2s/c2suiapi/C2sUiApiApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.samhsa.c2s.c2suiapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class C2sUiApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
