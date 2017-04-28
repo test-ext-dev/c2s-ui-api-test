c72a57a759026b0d4af5d8fa5ed0c1d886b0f308
Shruti Rao <shruti.rao@feisystems.com>
2017-04-28 19:51:49 -0400

    Add feign client annotation
    #13516:As a developer, I will have integrated try-my-policy API with C2S V3

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/TryPolicyClient.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/TryPolicyClient.java
@@ -1,10 +1,12 @@
 package gov.samhsa.c2s.c2suiapi.infrastructure;
 
 import gov.samhsa.c2s.c2suiapi.infrastructure.dto.TryPolicyDto;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@FeignClient("tryPolicy")
 public interface TryPolicyClient {
     @RequestMapping(value = "/tryPolicyXHTML", method = RequestMethod.GET)
     public TryPolicyDto tryPolicyByConsentIdXHTML(@RequestParam("documentId") String documentId,
