27e816e3b5fe87205d6399eab6425668782a492c
CindyLiFei <cindy.li@feisystems.com>
2017-05-01 13:03:39 -0400

    Move cast oAuth2AuthenticationDetails to JwtTokenExtractor service
    #13512: As a developer, I will have integrated UMS with C2S components

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/JwtTokenExtractor.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/JwtTokenExtractor.java
@@ -1,7 +1,7 @@
 package gov.samhsa.c2s.c2suiapi.service;
 
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 public interface JwtTokenExtractor  {
-    public String getValueByKey(OAuth2AuthenticationDetails oAuth2AuthenticationDetails, String key);
+    public String getValueByKey(OAuth2Authentication oAuth2Authentication, String key);
 }
--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/JwtTokenExtractorImpl.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/JwtTokenExtractorImpl.java
@@ -4,6 +4,7 @@ import org.springframework.security.jwt.Jwt;
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.common.util.JsonParser;
 import org.springframework.security.oauth2.common.util.JsonParserFactory;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,8 @@ import java.util.Map;
 
 @Service
 public class JwtTokenExtractorImpl implements JwtTokenExtractor{
-    public String getValueByKey(OAuth2AuthenticationDetails oAuth2AuthenticationDetails, String key){
+    public String getValueByKey(OAuth2Authentication oAuth2Authentication, String key){
+        OAuth2AuthenticationDetails oAuth2AuthenticationDetails=(OAuth2AuthenticationDetails)oAuth2Authentication.getDetails();
        JsonParser objectMapper = JsonParserFactory.create();
        Jwt jwt = JwtHelper.decode(oAuth2AuthenticationDetails.getTokenValue());
        String content = jwt.getClaims();
