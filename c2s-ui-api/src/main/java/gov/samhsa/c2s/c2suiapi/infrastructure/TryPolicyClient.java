3c07711f837c5cc707b39632c58566172c0e9c5b
Shruti Rao <shruti.rao@feisystems.com>
2017-05-17 16:14:23 -0400

    Remove extra tabs
    #13763:As a developer, I will have implemented the Try My Policy UI and integrated with backend [v3.0]

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/TryPolicyClient.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/TryPolicyClient.java
@@ -1,13 +1,13 @@
 package gov.samhsa.c2s.c2suiapi.infrastructure;
 
-        import gov.samhsa.c2s.c2suiapi.service.dto.TryPolicyResponse;
-        import org.springframework.cloud.netflix.feign.FeignClient;
-        import org.springframework.web.bind.annotation.RequestHeader;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestMethod;
-        import org.springframework.web.bind.annotation.RequestParam;
+import gov.samhsa.c2s.c2suiapi.service.dto.TryPolicyResponse;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-        import java.util.Locale;
+import java.util.Locale;
 
 @FeignClient("tryPolicy")
 public interface TryPolicyClient {
