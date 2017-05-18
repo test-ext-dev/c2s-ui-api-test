76534febf3f4b0a226467ccf0233d0136306fd98
Shruti Rao <shruti.rao@feisystems.com>
2017-05-18 13:15:26 -0400

    Add custom exceptions to Try-Policy
    #13763:As a developer, I will have implemented the Try My Policy UI and integrated with backend [v3.0]

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/TryPolicyServiceImpl.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/TryPolicyServiceImpl.java
@@ -1,7 +1,10 @@
 package gov.samhsa.c2s.c2suiapi.service;
 
+import feign.FeignException;
 import gov.samhsa.c2s.c2suiapi.infrastructure.TryPolicyClient;
 import gov.samhsa.c2s.c2suiapi.service.dto.TryPolicyResponse;
+import gov.samhsa.c2s.c2suiapi.service.exception.NoDocumentsFoundException;
+import gov.samhsa.c2s.c2suiapi.service.exception.TryPolicyException;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
@@ -16,6 +19,12 @@ public class TryPolicyServiceImpl implements TryPolicyService {
 
     @Override
     public TryPolicyResponse getSegmentDocXHTML(String documentId, String consentId, String patientId, String purposeOfUse, Locale locale) {
-        return tryPolicyClient.tryPolicyByConsentIdXHTML(documentId, consentId, patientId, purposeOfUse, locale);
+       try{
+           return tryPolicyClient.tryPolicyByConsentIdXHTML(documentId, consentId, patientId, purposeOfUse, locale);
+       }catch (FeignException fe){
+           if(fe.status() == 404){
+               throw new NoDocumentsFoundException(fe.getMessage());
+           } else throw new TryPolicyException("Unknown Exception");
+       }
     }
 }
