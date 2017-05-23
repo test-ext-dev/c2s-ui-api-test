b648e1b9540710cb0c0199f96402438eeb720e6e
Shruti Rao <shruti.rao@feisystems.com>
2017-05-23 17:13:36 -0400

    Split into declaration and assignment
    #13763:As a developer, I will have implemented the Try My Policy UI and integrated with backend [v3.0]

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/TryPolicyServiceImpl.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/TryPolicyServiceImpl.java
@@ -1,6 +1,7 @@
 package gov.samhsa.c2s.c2suiapi.service;
 
 import gov.samhsa.c2s.c2suiapi.infrastructure.TryPolicyClient;
+import gov.samhsa.c2s.c2suiapi.service.exception.TryPolicyException;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
@@ -15,6 +16,13 @@ public class TryPolicyServiceImpl implements TryPolicyService {
 
     @Override
     public Object getSegmentDocXHTML(String documentId, String consentId, String patientId, String purposeOfUse, Locale locale) {
-          return tryPolicyClient.tryPolicyByConsentIdXHTML(documentId, consentId, patientId, purposeOfUse, locale);
+        Object tryPolicyResponse;
+        try{
+            tryPolicyResponse = tryPolicyClient.tryPolicyByConsentIdXHTML(documentId, consentId, patientId, purposeOfUse, locale);
+        }catch (Exception e){
+            //Todo: Refactor this block to watch for specific error status codes and throw custom exception
+            throw new TryPolicyException(e.getMessage());
+        }
+        return tryPolicyResponse;
     }
 }
