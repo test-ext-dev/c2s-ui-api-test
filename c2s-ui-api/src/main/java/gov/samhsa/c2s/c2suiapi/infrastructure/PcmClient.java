43dc6eb00c6a86d3bd92f1af439e2e784f8799ff
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-06-28 08:55:58 -0400

    Change all package import to specific import
    #14280 - As a developer I will make consent share/not share type configurable at Deployment Time to support patient UI - Backend

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/PcmClient.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/PcmClient.java
@@ -1,6 +1,15 @@
 package gov.samhsa.c2s.c2suiapi.infrastructure;
 
-import gov.samhsa.c2s.c2suiapi.infrastructure.dto.*;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentProviderDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.IdentifiersDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.DetailedConsentDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.PageableDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentAttestationDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentRevocationDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentTypeConfigurationDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.PurposeDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentTermDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/PcmService.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/PcmService.java
@@ -1,6 +1,15 @@
 package gov.samhsa.c2s.c2suiapi.service;
 
-import gov.samhsa.c2s.c2suiapi.infrastructure.dto.*;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentProviderDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.IdentifiersDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.DetailedConsentDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.PageableDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentAttestationDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentRevocationDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentTypeConfigurationDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.PurposeDto;
+import gov.samhsa.c2s.c2suiapi.infrastructure.dto.ConsentTermDto;
 
 import java.util.List;
 import java.util.Locale;
