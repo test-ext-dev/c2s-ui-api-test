b72f82dca0264d46a7b271e3bcb88a65021dcbcb
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-07-20 21:17:31 -0400

    Add userId field to LimitedProfileResponse class
    #14601 - As a developer I will create a profile page for patients to view/update their profile info, with the option of also uploading an avatar

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/UmsServiceImpl.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/UmsServiceImpl.java
@@ -88,6 +88,7 @@ public class UmsServiceImpl implements UmsService {
         UmsUserDto currentUser = umsClient.getUserByAuthId(userAuthId);
 
         return LimitedProfileResponse.builder()
+                .userId(currentUser.getId())
                 .userLocale(currentUser.getLocale())
                 .supportedLocales(supportedLocales)
                 .username(currentUsername)
